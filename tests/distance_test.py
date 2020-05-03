f9417c28e230c0a2f6b452bc92cc449994645fc6
lukas <lukas.b.franken@gmail.com>
2020-05-03 16:53:07 +0000

    changes

--- tests/distance_test.py
+++ tests/distance_test.py
@@ -1,13 +1,15 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from collections import OrderedDict
 import sys
 import numpy as np
 sys.path += ['../']
 sys.path += ['../utils/']
-from models.cifar10.models import Wide_ResNet
+from models.cifar10.models import Wide_ResNet, resnet110
 import config as cf
 from attacks import *
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 def mean(liste):
     summe = 0.
@@ -16,17 +18,34 @@ def mean(liste):
     return summe/len(liste)
 
 
-device = torch.device("cuda:0")
+device = torch.device("cuda")
 num_examples = 1000
 batch_size = 100
 eps = 1.
-alpha = 1e-3
-max_iter = 10000
+alpha = 1e-4
+max_iter = 100000
 steps = np.zeros(num_examples)
-adv = Adversary('fgsm', torch.device('cuda:0'))
-model_address = '../models/cifar10/trained_models/noisy_0.4_WideResNet_28_10_run_1.pth'
+adv = Adversary('fgsm', device)
+model_address = '../models/cifar10/trained_models/normal_WideResNet_28_10_run_1.pth'
+#model_address = '../models/cifar10/trained_models/ResNet_110.th'
+
+
+state_dict = torch.load(model_address)
+
+"""
+####### the following is for ResNet loading only #####
+new_state_dict = OrderedDict()
+for k, v in state_dict['state_dict'].items():
+    name = k[7:] 
+    new_state_dict[name] = v
+######################################################
+"""
+
 model = Wide_ResNet(28, 10, 0.3, 10).to(device)
-model.load_state_dict(torch.load(model_address))
+#model = resnet110().to(device)
+#model.load_state_dict(torch.load(model_address))
+model.load_state_dict(state_dict)
+#model.load_state_dict(new_state_dict)
 model.eval()
 
 transform = transforms.Compose([
@@ -50,6 +69,7 @@ for i in range(int(num_examples/batch_size)):
                                 eps=eps, alpha=alpha, max_iter=max_iter)
     distances += hold.tolist()
     step_counter += steps.tolist()
+    print('recently found distances: ', hold.tolist())
     print('in eps {} iteration {}/{} with mean {}'.format(eps, i+1, int(num_examples/batch_size), mean(distances[-batch_size:])))
 
 print(distances)
