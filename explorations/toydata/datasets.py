10f2297db9c1ba3965a81cc6d1535221f425aeef
bogdan <bogdan.m.georgiev@gmail.com>
2020-05-13 18:21:56 +0200

    update toydata

---
+++ explorations/toydata/datasets.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset
+
+class Star(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
