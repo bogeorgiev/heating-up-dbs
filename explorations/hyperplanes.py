23cfc17b9e34166c5574d2f70e7f37e4022793c1
bogdan <bogdan.m.georgiev@gmail.com>
2020-05-31 16:27:44 +0200

    add plotting of hyperplane stats

--- explorations/hyperplanes.py
+++ explorations/hyperplanes.py
@@ -10,28 +10,6 @@ import numpy as np
 from scipy.interpolate import make_interp_spline
 from utils.utils import planar_cap
 
-dim = 3 * 32 * 32 #CIFAR10 dim
-alpha = 1.0e0
-num_walks = 20000
-num_steps = 100 * alpha
-step = 0.1 / math.sqrt(alpha)
-c = 0.05
-t = torch.tensor(num_steps * step**2)
-
-compute_empirical_cap = False
-compute_empirical_vol = True 
-
-normal_1d = Normal(torch.tensor(0.0), torch.tensor(1.0))
-
-sample = torch.zeros(dim)
-msd = .0
-msd_th = step * math.sqrt( num_steps * dim)
-radius = msd_th
-
-
-print("BM run for time: ", t)
-print("RMSD (theoretical): ", msd_th)
-
 
 def compute_vol(h, compute_empirical_vol, num_samples=500000, dim=3072):
     if (compute_empirical_vol):
@@ -51,6 +29,7 @@ def compute_vol(h, compute_empirical_vol, num_samples=500000, dim=3072):
 
 def compute_cap(h, compute_empirical_cap, dim=3072, num_walks=1000):
     t = torch.tensor(1.0 / dim)
+    normal_1d = Normal(torch.tensor(0.0), torch.tensor(1.0))
     hits = 0
     if (compute_empirical_cap):
         for i in range(num_walks):
@@ -96,27 +75,42 @@ def compute_mesh(compute_empirical_vol, compute_empirical_cap, dim=3072, dist=0.
         print("MSD (empirical): ", msd)
         print("Hitting Prob (empirical): ", hits / num_walks)
 
+    np.save("xs_dim"+str(dim), xs)
+    np.save("ys_dim"+str(dim), ys)
     return xs, ys
 
 def plot_tau_stats():
-    #dims = [10, 20, 30, 40, 50]
-    dims = [3060, 3070, 3080, 3090, 4000]
+    dims = [10, 20, 30, 40, 50]
     fig = plt.figure()
     ax = fig.add_subplot()
     ax.set_title("Isocapacitory Saturation for a Hyperplane")
     ax.set_ylabel("Saturation")
     ax.set_xlabel("Distance from starting point to hyperplane")
     for dim in dims:
-        xs, ys = compute_mesh(compute_empirical_vol, compute_empirical_cap, dist=0.5, dim=dim, mesh_size=7)
+        xs, ys = compute_mesh(compute_empirical_vol=True, compute_empirical_cap=False, dist=0.5, dim=dim, mesh_size=7)
         ax.plot(xs, ys, label=str(dim)) 
 
     labelLines(plt.gca().get_lines(), xvals=(0.2, 0.5), zorder=2.5)
     plt.show()
 
+def main():
+    dim = 3 * 32 * 32 #CIFAR10 dim
+    alpha = 1.0e0
+    num_steps = 100 * alpha
+    step = 0.1 / math.sqrt(alpha)
+    t = torch.tensor(1.0 / dim)
 
-if __name__=="__main__":
-    dist = 0.042
-    dim = 3072
+    sample = torch.zeros(dim)
+    msd = .0
+    msd_th = math.sqrt(t * dim)
+    radius = msd_th
+
+    print("BM run for time: ", t)
+    print("RMSD (theoretical): ", msd_th)
     #print("Cap ", compute_cap(dist, compute_empirical_cap))
     #print("Vol ", compute_vol(dist, compute_empirical_vol))
+
     plot_tau_stats()
+
+if __name__=="__main__":
+    main()
